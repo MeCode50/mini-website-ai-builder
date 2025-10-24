cd3eb5eaeadd62aae4902f35a6c60ffc78582434
MeCode50 <victorycollins31@gmail.com>
2025-10-24 01:21:33 +0100

    changed from neon to prisms

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
+import { AiModule } from '../ai/ai.module';
 
 @Module({
+  imports: [AiModule],
   controllers: [HealthController],
   providers: [HealthService],
 })
